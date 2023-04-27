aca1d14b2a600b7cb9d418884c56b46c5bc1210f
AngelVelasco <angelgg2020@outlook.com>
2023-04-27 09:19:23 -0500

    feat: :tada: Add new examples of web components

--- script.js
+++ script.js
@@ -6,5 +6,23 @@ class dogListComponent extends HTMLElement {
         this.innerHTML = `The dog is ${this.name}`
     }
 }
+customElements.define('dog-list', dogListComponent);
 
-customElements.define('dog-list', dogListComponent)
+class customButton extends HTMLButtonElement {
+    constructor() {
+        super();
+        this.addEventListener('click', () => {
+            alert("Hi from custom button")
+        });
+    }
+}
+customElements.define('console-button', customButton, {extends: 'button'});
+
+class gallery extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({mode: 'open'});
+
+    }
+}
